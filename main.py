9bd3fcd1bd4b68bc8bffaac9e66f8cac94323abf
Eugen Ajechiloae <eugen.ajechiloae@clear.ml>
2023-06-11 14:55:19 +0300

    save

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import gradio as gr
 import numpy as np
 from clearml.binding.gradio_bind import PatchGradio
 import clearml
+import gradio.routes
 
 
-task = clearml.Task.current_task()
+if __name__ == "__main__":
+    clearml.Task.init()
+    task = clearml.Task.current_task()
 
-print(clearml.__version__)
-print(PatchGradio._current_task)
-
-
-def image_generator(text):
-    return np.random.randint(0, 255, size=(244, 244, 3))
+    print(clearml.__version__)
+    print(PatchGradio._current_task)
 
 
-with gr.Blocks() as demo:
-    button = gr.Button(label="Generate Image")
-    button.click(fn=image_generator, inputs=gr.Textbox(), outputs=gr.Image())
+    def image_generator(text):
+        return np.random.randint(0, 255, size=(244, 244, 3))
 
-demo.queue(concurrency_count=3)
-demo.launch(server_name="0.0.0.0")  #, root_path=f"/service/{task.id}")
+
+    with gr.Blocks() as demo:
+        button = gr.Button(label="Generate Image")
+        button.click(fn=image_generator, inputs=gr.Textbox(), outputs=gr.Image())
+
+    demo.queue(concurrency_count=3)
+    demo.launch(server_name="0.0.0.0")  #, root_path=f"/service/{task.id}")
